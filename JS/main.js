141008958be35884bf9c098f4d14f5da0ea346d9
KhalilNiser <kaliniser@gmail.com>
2024-09-26 13:09:58 -0700

    functions_project

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+
+// Declare a function myMethod
+function myMethod()
+{
+    // Concatenate two quotes
+    var quote = "It's Better to Burnout.";
+    quote = quote + " Than to Fade Away...!"
+
+    document.getElementById( "concatenate" ).innerHTML = quote;
+}
